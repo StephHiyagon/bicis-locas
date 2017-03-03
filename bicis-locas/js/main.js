1cb2055d8190ca3b6c70359d9c634ae6822012be
stephHiyagon <mv.hiyagon@gmail.com>
2017-03-03 14:25:45 -0500

    con validacion de reg-exp

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -18,15 +18,15 @@ function validateForm(){
     alert("todos los campos son obligatorios");
     return false;
     }
-  else if (!(/^[A-Z][a-z]*/.test(nombre)) ) {
+  else if (!(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/.test(nombre)) ) {
     alert("En el NOMBRE:'Sólo se permiten caracteres de la a- z, la primera letra debe ser mayúscula'");
     return false;
     }
-  else if (!(/^[A-Z][a-z]*/.test(apellido)) ) {
+  else if (!(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/.test(apellido)) ) {
       alert("En el APELLIDO:'Sólo se permiten caracteres de la a- z, la primera letra debe ser mayúscula'");
       return false;
       }
-  else if (!(/\w+@\w+\.+[a-z]/.test(correo))) {
+  else if (!(/[\w]+@{1}[\w]+\.[a-z]{2,3}/.test(correo))) {
       alert("El correo no tiene un formato valido");
       return false;
     }
@@ -42,7 +42,7 @@ function validateForm(){
   document.getElementById('lastname').value="";
   document.getElementById('input-email').value="";
   document.getElementById('input-password').value="";
-  document.getElementById('bici').value="";
+  document.getElementById('bici').value=0;
   document.getElementById('input-social').value="";
-
+  return true;
 }
