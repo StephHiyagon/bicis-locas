3385f051159404a7fccba9c1f9c8f65825037276
StephHiyagon <mv.hiyagon@gmail.com>
2017-03-03 00:46:31 -0500

    validado campos obligatorios, contrasena y correo

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -1,30 +1,38 @@
-var nombre=document.getElementById('name').value;
-var apellido=document.getElementById('lastname').value;
-var correo=document.getElementById('inputEmail').value;
-var contrasena=document.getElementById('inputPassword').value;
-var escoger=document.getElementById('bici').selectedIndex;
 
-function valida(nombre,apellido,correo,contrasena,escoger){
+function validateForm(){
+  var nombre=document.getElementById('name').value;
+  var apellido=document.getElementById('lastname').value;
+  var correo=document.getElementById('input-email').value;
+  var contrasena=document.getElementById('input-password').value;
+  var indice=document.getElementById('bici').selectedIndex;
+  var twit=document.getElementById('input-social').value;
+  console.log(nombre);
+  console.log(apellido);
+  console.log(correo);
+  console.log(contrasena);
+  console.log(indice);
+
+
 	/* Escribe tú código aquí */
-  if(nombre==null || nombre.length==0 || !(^\d*$/.test(nombre)){
-    alert("error");
+  if(nombre==="" || apellido ==="" || correo ==="" || contrasena ==="" || indice === 0 || twit==="" ){
+    alert("todos los campos son obligatorios");
+    return false;
+    }
+  else if (!(/^[A-Z]+[a-z]$/.test(nombre)) ) {
+    alert("Sólo se permiten caracteres de la a- z, la primera letra debe ser mayúscula");
+    return false;
+    }
+
+  else if (!(/\w+@\w+\.+[a-z]/.test(correo))) {
+      alert("El correo no tiene un formato valido");
+      return false;
+    }
+  else if (contrasena.length<=5) {
+    alert("la contraseña debe tener mínimo 6 caracteres");
     return false;
   }
-  else if(apellido==null || apellido.length==0 || !(^\d*$/.test(apellido))){
-    alert("error");
+  else if (contrasena==="password" || contrasena==="123456" || contrasena==="098754") {
+    alert("la contraseña no es valida");
     return false;
   }
-  else if( escoger == null || escoger == 0 ) {
-    alert("error");
-    return false;
-  }
-  else if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(correo)) ) {
-    alert("error");
-    return false;
-  }
-  else if( !(/^\d{6}$/.test(contrasena)) || contrasena=="password" || contrasena==123456 || contrasena==098754 ) {
-    alert("error");
-    return false;
-}
-  return true;
 }
